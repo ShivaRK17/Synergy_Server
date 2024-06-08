4d35667ff2a39a31b614dd4ab926e9c34e393daf
ShivaRK <shivarkcodes@gmail.com>
2024-06-08 18:57:05 +0530

    fix:cors2

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const port = process.env.PORT
 const cors = require('cors')
 app.use(cors({
     origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization','auth-token'],
-    credentials: true
-}))
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
+}));
 require('./db/conn')
 
 app.use(express.json())
-app.use('/api/auth',require('./routes/auth'))
+
+app.get('/',(req,res)=>{
+    res.json({"message":"Welcome to Synergy"})
+})
+app.use('/api/auth', require('./routes/auth'))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
