7d85918f3574f89eff0e0bb595e53c7f52d1f2db
ShivaRK <shivarkcodes@gmail.com>
2024-06-08 16:46:07 +0530

    fix:cors

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const app = express()
 require('dotenv').config()
 const port = process.env.PORT
 const cors = require('cors')
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization','auth-token'],
+    credentials: true
+}))
 require('./db/conn')
 
 app.use(express.json())
